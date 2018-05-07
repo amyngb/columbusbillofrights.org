4aeffb3649d08bc9cf0351b3b9b2ebe455712614
amyngb <amyngbedinghaus@gmail.com>
2018-05-07 19:58:39 -0400

    Google static map

---
+++ CCBOR.Web/Content/assets/js/googlemap.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    
+    var env = {
+        staticMapKey: '',
+    };
+
+    // Import variables if present (from env.js)
+    if (window.env) {
+        Object.assign(env, window.env);
+    }
+
+    setTimeout(function () {
+        var mapSrc = 'https://maps.googleapis.com/maps/api/staticmap?center=2101+Integrity+Dr+S+Columbus+OH+43209&zoom=12&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C2101+Integrity+Dr+S+Columbus+OH+43209&key=' + env.staticMapKey;
+
+        $("#staticMap").attr("src", mapSrc);
+    }, 300);
+   
+
+});
