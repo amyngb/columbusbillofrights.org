9fe058509d86bc80a774dee9559577a41270f772
amyngb <amyngbedinghaus@gmail.com>
2018-05-08 09:35:33 -0400

    Google map responsive and shared view

--- CCBOR.Web/Content/assets/js/googlemap.js
+++ CCBOR.Web/Content/assets/js/googlemap.js
@@ -10,7 +10,7 @@
     }
 
     setTimeout(function () {
-        var mapSrc = 'https://maps.googleapis.com/maps/api/staticmap?center=2101+Integrity+Dr+S+Columbus+OH+43209&zoom=12&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C2101+Integrity+Dr+S+Columbus+OH+43209&key=' + env.staticMapKey;
+        var mapSrc = 'https://maps.googleapis.com/maps/api/staticmap?center=2500+Waters+Edge+Blvd+Columbus+OH+43209&zoom=12&size=500x300&maptype=roadmap&markers=color:red%7Clabel:A%7C2101+Integrity+Dr+S+Columbus+OH+43209&key=' + env.staticMapKey;
 
         $("#staticMap").attr("src", mapSrc);
     }, 300);
