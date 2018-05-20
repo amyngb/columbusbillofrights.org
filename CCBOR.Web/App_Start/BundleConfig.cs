5ab047c28ca6751fb73a4bf28bc5b840d77cc068
JMG Software <jgildone@jmgsoftware.com>
2018-05-20 16:54:17 -0400

    Latest Changes

--- CCBOR.Web/App_Start/BundleConfig.cs
+++ CCBOR.Web/App_Start/BundleConfig.cs
@@ -8,24 +8,42 @@ namespace CCBOR.Web
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            #region Scripts
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/jQueryCore").Include(
+                        "~/Content/assets/plugins/jquery/jquery-3.3.1.min.js"
+                        ));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jQueryFeatures").Include(
+                     "~/Content/assets/plugins/form.validate/jquery.form.min.js",
+                     "~/Content/assets/plugins/form.validate/jquery.validation.min.js",
+                     "~/Content/assets/plugins/toastr/toastr.js"
+                     ));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/siteScripts").Include(
+                     "~/Content/assets/js/scripts.js",
+                     "~/Scripts/Custom/Common.js"
+                     ));
+
+            #endregion
+
+            #region Styles
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap")
+                        .Include("~/Content/assets/plugins/bootstrap/css/bootstrap.min.css")
+                        );
+
+            bundles.Add(new StyleBundle("~/Content/core")
+               .Include("~/Content/assets/css/essentials.css", new CssRewriteUrlTransform())
+               .Include("~/Content/assets/css/layout.css", new CssRewriteUrlTransform())
+               .Include("~/Content/assets/css/ccbor.css", new CssRewriteUrlTransform())
+               .Include("~/Content/assets/css/header-1.css", new CssRewriteUrlTransform())
+               .Include("~/Content/assets/css/color_scheme/blue.css", new CssRewriteUrlTransform())
+               );
+
+            #endregion
+
         }
     }
 }
